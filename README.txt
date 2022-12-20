20f602ce11ff12c1e61407c340e4cfe37f8d0568
Dabija Vlad <dabija_vlad_adrian@yahoo.com>
2022-12-20 08:18:38 +0200

    Fixed 2 bugs that i identified at a later test of the game. Added one more game mode and transitions to X's and 0's

--- README.txt
+++ README.txt
@@ -2,4 +2,15 @@ Tic-Tac-Toe project.
 
 TheOdinProject path lead me to this awesome project idea. 
 
-I'll come back to write the skills i learned and what i got to exercise building this. 
+What i learned?
+
+-To commit more often even though i did not commit often enough
+-To use factory functions and modules
+-Got to practice a little bit with the transitions (on CSS part)
+-Testing the product again in the next day (or after a little break) is good at finding bugs
+
+
+I really enjoyed making this project. There are still things to be improved,
+making more functions and use them where the code repeats, maybe use a pattern instead of 20 -if-
+and an upgrade on the style part, maybe find a way to use the setTimeout too. But 
+when i will make it more responsive, it will be a challenge to work on my past code and ill leave it to that moment.
--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const gameBoard = (() => {
     const isWin = (box) => {
         if(box[0] === 1 && box[1] === 1 && box[2] === 1)
         {
-            winner = 'player1';
+            winner = 1;
             return winner;
         }
         else if(box[0] === 1 && box[3] === 1 && box[6] === 1)
@@ -112,20 +112,26 @@ const gameBoard = (() => {
         let turns = 0;
         const handleClick = (e) => {
             let boxNumber = parseInt(e.target.className[4]);
-            turns ++;
             const target = e.target;
+            if(target.textContent==='X' || target.textContent==='0')
+            {
+                return;
+            }
+            turns ++;
             if(gameType() === 'player')
             {
-                if(player1.turn === true && winner === null && target.classList.contains("circle")!==true)
+                if(player1.turn === true && winner === null && target.textContent!=='0')
                 {
-                    target.classList.add("cross");
+                    target.textContent = "X";
+                    target.style.color = '#FF4500';
                     boxArr[boxNumber] = player1.value;
                     player1.turn = false;
                     player2.turn = true;
                 }
-                else if (player2.turn === true && winner === null && target.classList.contains("cross")!==true)
+                else if (player2.turn === true && winner === null && target.textContent!=='X')
                 {
-                    target.classList.add("zero");
+                    target.textContent = "0";
+                    target.style.color = '#FF4500';
                     boxArr[boxNumber] = player2.value;
                     player1.turn = true;
                     player2.turn = false;
@@ -157,26 +163,19 @@ const gameBoard = (() => {
                     }
                 }
             }
-            else {
-                if(winner === null && target.classList.contains('cross')!== true)
+            else if(gameType() === 'easy'){
+                if(winner === null && target.textContent!=='X' && target.textContent!=='0')
                 {
-                    target.classList.add('cross');
+                    target.textContent = "X";
+                    target.style.color = '#FF4500';
                     boxArr[boxNumber] = player1.value;
                     turns++;
-                    let compMove = Math.floor(Math.random()*8)+1;
-                    /*if(compMove === boxNumber)
-                    {
-                        while(compMove === boxNumber)
-                        {
-                            compMove = Math.floor(Math.random()*8)+1;
-                        }
-                    }*/
                     [...cellElements].some ((cell) => {
-                        if(!cell.classList.contains('cross') && !cell.classList.contains('zero'))
+                        if(cell.textContent!=='X' && cell.textContent!=='0')
                         {
-                            console.log('ag');
                             boxArr[parseInt(cell.className[4])] = 0;
-                            cell.classList.add('zero');
+                            cell.textContent = "0";
+                            cell.style.color = '#FF4500'
                             return true;
                         }
                         return false;
@@ -208,6 +207,55 @@ const gameBoard = (() => {
                     }
                 }
             }
+            else
+            {
+            if(winner === null && target.textContent!=='X' && target.textContent!=='0')
+                {
+                    target.textContent = 'X';
+                    target.style.color = '#FF4500';
+                    boxArr[boxNumber] = player1.value;
+                    turns++;
+                    console.log(turns);
+                    let compMove = Math.floor(Math.random()*8)+1;
+                    while(boxArr[compMove]!== undefined && turns<9)
+                    {
+                        compMove = Math.floor(Math.random()*8)+1;
+                    }
+                    boxArr[compMove] = 0;
+                    cellElements.forEach (cell =>{
+                        if(parseInt(cell.className[4]) === compMove)
+                        {
+                            cell.textContent = '0';
+                            cell.style.color = '#FF4500';
+                        }
+                    })
+            }
+            if(turns>=5)
+                {
+                    if(isWin(boxArr) !=0 && isWin(boxArr) !=1 && turns !== 9)
+                    {
+                        return;
+                    }
+                    else if(turns === 9)
+                    {
+                        overlay.classList.add("show");
+                        winnerMessage.textContent = "Is a draw!";
+                        turns=0;
+                    }
+                    else if(isWin(boxArr) === 1)
+                    {
+                        overlay.classList.add("show");
+                        winnerMessage.textContent = "X wins!";
+                        turns=0;
+                    }
+                    else 
+                    {
+                        overlay.classList.add("show");
+                        winnerMessage.textContent = "0 wins!";
+                        turns=0;
+                    }
+                }
+            }
         }
         cellElements.forEach (cell => {
             cell.addEventListener('click', handleClick)
@@ -219,8 +267,8 @@ const gameBoard = (() => {
 
     restartBtn.addEventListener('click', (e) =>{
         playerTurn.cellElements.forEach (cell => {
-            cell.classList.remove("cross");
-            cell.classList.remove("zero");
+            cell.textContent='';
+            cell.style.color = '#ADD8E6';
         })
         boxArr = [];
         player1.turn = true;
@@ -233,4 +281,3 @@ const gameBoard = (() => {
     
     return {playerTurn};
 })();
-
